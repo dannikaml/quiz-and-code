1381ccec04ed48f55d346c5aef52355355e334c2
dannikaml <dannikaml@hotmail.com>
2023-01-08 10:48:34 -0600

    add .js button, question/answers; add css to stlye quiz; html questions text and href links

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,40 @@
-var generateBtn = document.querySelector("#generate");
+// Variables set to start the quiz (start button) 
+var startBtn = document.querySelector("#start");
+
+startBtn.addEventListener("click", startQuiz);
 
  function startQuiz() {
-  console.log("button clicked");
+  console.log("button clicked")
+  startBtn.classList.add('hide')
+  questionsContainerElement.classList.remove('hide')
 
 }
 
-generateBtn.addEventListener("click", startQuiz);
 
-//function beginQuiz() {
-   // var quiz = takeQuiz();
-   // var quizText = document.querySelector("#start");
-  
-   // quizText.value = quiz;
-//}
+//Variable to set questions, next questions, and answers
+var questionsContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerElement = document.getElementById('answer')
+
+
+//Retrieve next question 
+function nextQuestion(question) {
+      
+}
+
+
+//Select Answer
+function selectAnswer() {
+
+}
+
+//Questions
+var questions = [
+ {
+   question: 'Which data type is NOT supported by JavaScript?',
+   answers: [
+      { text: 'marsh', correct: true },
+      { text: 'boolean, string, array'}
+   ]
+ }
+]
