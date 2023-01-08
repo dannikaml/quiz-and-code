1e1ba8b733d3a9de8f2e2b17e0f310733ef94e18
dannikaml <dannikaml@hotmail.com>
2023-01-08 11:39:55 -0600

    add sources to readme, add questions to script.js

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # quiz-n-code
+
+## Description - How much do you know about JavaScript?
+***
+quiz 'n' code is a website housing a JavaScript based quiz for any and all who want to test their JavaScript knowledge. A ninty second timer runs and for each incorrect answer fifteen seconds is subtracted from the overall timing. At the end of each quiz highscores are saved and you can find them by clicking on the highscores button. 
+
+***
+### Usage
+To run a timed quiz based on javaScript and to store highscores. 
+
+
+
+
+### Sources
+***
+Youtube:
+ - https://www.youtube.com/watch?v=riDzcEQbX6k
+
+ w3schools:
+  - https://www.w3schools.com/html/html_basic.asp
+  - https://www.w3schools.com/css/css3_buttons.asp
+  - https://www.w3schools.com/css/css_list.asp
+
+ edureka:
+  - https://www.edureka.co/blog/interview-questions/javascript-interview-questions/#beginner
--- assets/script.js
+++ assets/script.js
@@ -6,20 +6,33 @@ startBtn.addEventListener("click", startQuiz);
  function startQuiz() {
   console.log("button clicked")
   startBtn.classList.add('hide')
+  randomQuestion = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
   questionsContainerElement.classList.remove('hide')
+  submitQuestion()
 
 }
 
 
 //Variable to set questions, next questions, and answers
 var questionsContainerElement = document.getElementById('question-container')
-var questionElement = document.getElementById('question')
+var questionElement = document.getElementById('questions')
 var answerElement = document.getElementById('answer')
+
+//setting an index for the questions and setting them to appear randomly
+let randomQuestion, currentQuestionIndex
 
 
 //Retrieve next question 
-function nextQuestion(question) {
+function submitQuestion() {
+   showQuestion(randomQuestion[currentQuestionIndex])
+
       
+}
+
+function showQuestion(questions) {
+   questionElement.innerText = questions.questions
+
 }
 
 
@@ -31,10 +44,10 @@ function selectAnswer() {
 //Questions
 var questions = [
  {
-   question: 'Which data type is NOT supported by JavaScript?',
+   questions: 'Which data type is NOT supported by JavaScript?',
    answers: [
       { text: 'marsh', correct: true },
-      { text: 'boolean, string, array'}
+      { text: 'boolean, string, array', correct: false}
    ]
  }
 ]
